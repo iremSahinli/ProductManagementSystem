e120a43694fbb4d00d4ea4676f5dec95a92e86e9
İrem Şahinli <160491598+iremSahinli@users.noreply.github.com>
2024-08-30 00:30:46 +0300

    Infrastructure DataAccess  için Interface ve Concretes sınıfları eklendi.

---
+++ ManagmentSystem.Infrastructure/DataAccess/Interfaces/IAsyncFindableRepository.cs
@@ -0,0 +1,17 @@
+﻿using ManagmentSystem.Domain.Core.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagmentSystem.Infrastructure.DataAccess.Interfaces
+{
+    public interface IAsyncFindableRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> exception = null);
+        Task<TEntity?> GetByIdAsync(Guid id, bool tracking = true);
+        Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> exception, bool tracking = true);
+    }
+}
---
+++ ManagmentSystem.Infrastructure/DataAccess/Interfaces/IAsyncInsertableRepository.cs
@@ -0,0 +1,15 @@
+﻿using ManagmentSystem.Domain.Core.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagmentSystem.Infrastructure.DataAccess.Interfaces
+{
+    public interface IAsyncInsertableRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entity); // Dönüş yok
+    }
+}
