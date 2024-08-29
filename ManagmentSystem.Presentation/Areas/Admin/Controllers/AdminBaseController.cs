cbb92ff5812a6475ace52f4b180695d37ce481a3
dilarabilkay <dilarabilkay645@gmail.com>
2024-08-29 20:46:30 +0300

    Domain, Presentation, Infra katmanları oluşturuldu. Admin için gereken tüm yapılar oluşturuldu.

---
+++ ManagmentSystem.Presentation/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagmentSystem.Presentation.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminBaseController
+    {
+    }
+}
