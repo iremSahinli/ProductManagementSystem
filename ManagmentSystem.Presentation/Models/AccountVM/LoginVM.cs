f79a5c380cfb43bb8df27857b2037a157417bae7
mberberci <mehmet.berberci.mb@hotmail.com>
2024-09-05 19:09:06 +0300

    Sayfaların ve butonların düzeltilmesi işlemi yapılmıştır.

--- ManagmentSystem.Presentation/Models/AccountVM/LoginVM.cs
+++ ManagmentSystem.Presentation/Models/AccountVM/LoginVM.cs
@@ -1,8 +1,14 @@
-﻿namespace ManagmentSystem.Presentation.Models.AccountVM
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagmentSystem.Presentation.Models.AccountVM
 {
     public class LoginVM
     {
+        [Required(ErrorMessage ="Email is required.")]
+        [EmailAddress(ErrorMessage ="Invalid email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
         public string Password { get; set; }
     }
 }
