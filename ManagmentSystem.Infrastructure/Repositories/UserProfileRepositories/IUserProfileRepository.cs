a2606cc36db12f72b17801e925955ac5e0c6b836
İrem Şahinli <160491598+iremSahinli@users.noreply.github.com>
2024-09-29 02:12:19 +0300

    Uygulama kontrol edildi, service metotları için summaryler eklendi, hataların düzeltilmesi için kodlarda değişiklikler yapıldı.

--- ManagmentSystem.Infrastructure/Repositories/UserProfileRepositories/IUserProfileRepository.cs
+++ ManagmentSystem.Infrastructure/Repositories/UserProfileRepositories/IUserProfileRepository.cs
@@ -10,6 +10,14 @@ namespace ManagmentSystem.Infrastructure.Repositories.UserProfileRepositories
 {
     public interface IUserProfileRepository : IAsyncRepository, IAsyncFindableRepository<UserProfile>, IAsyncInsertableRepository<UserProfile>, IAsyncQueryableRepository<UserProfile>, IAsyncDeletableRepository<UserProfile>, IAsyncUpdatebleRepository<UserProfile>, IAsyncTransactionRepository
     {
+        /// <summary>
+        /// Belirtilen kimliğe (identityUserId) göre kullanıcı profilini bulur.
+        /// </summary>
+        /// <param name="identityUserId">Aranacak kullanıcının kimliği.</param>
+        /// <returns>
+        /// Kullanıcı profilini içeren bir UserProfile nesnesi döndüren asenkron bir görev. 
+        /// Kullanıcı bulunamazsa null dönebilir.
+        /// </returns>
         Task<UserProfile?> FindByIdentityUserIdAsync(string identityUserId);
     }
 }
--- ManagmentSystem.Presentation/Areas/User/Controllers/UserController.cs
+++ ManagmentSystem.Presentation/Areas/User/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using ManagmentSystem.Business.Services.UserProfileServices;
 using ManagmentSystem.Presentation.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using System.Security.Claims;
 
 namespace ManagmentSystem.Presentation.Areas.User.Controllers
@@ -14,11 +15,13 @@ namespace ManagmentSystem.Presentation.Areas.User.Controllers
 
         private readonly IProductService _productService;
         private readonly IUserProfileService _userProfileService;
+        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
 
-        public UserController(IProductService productService, IUserProfileService userProfileService)
+        public UserController(IProductService productService, IUserProfileService userProfileService, IStringLocalizer<SharedResources> stringLocalizer)
         {
             _productService = productService;
             _userProfileService = userProfileService;
+            _stringLocalizer = stringLocalizer;
         }
 
         public async Task<IActionResult> GetProducts()
@@ -49,7 +52,8 @@ namespace ManagmentSystem.Presentation.Areas.User.Controllers
 
             // Hata durumunda bir View dönebilirsiniz veya hata mesajı gösterebilirsiniz
             ErrorNotyf("Ürünler Listeleme Başarısız");
-            return View("Error");
+            return RedirectToAction("Index", "User", new { area = "" });
+
         }
 
 
